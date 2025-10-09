9275d6f601a570ce258fd75a62dd8b8a187a3c11
Mario Reder <mario.reder@pm.me>
2025-10-09 09:10:15 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -25,7 +25,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "https://x.com/i/communities/1972762204895117761",
   "VITE_SEO_SITE_NAME": "Fartliquid",
   "VITE_SEO_SITE_DESCRIPTION": "perps powered by natural ass energy",
-  "VITE_SEO_SITE_URL": "https://dex.orderly.network/perphub-5550/",
+  "VITE_SEO_SITE_URL": "https://fartliquidfun.xyz",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "@yildirimonsol",
